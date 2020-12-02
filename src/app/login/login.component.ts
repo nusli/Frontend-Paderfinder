df8ae23c0614f05556e7a1fff61ac598855fdb77
Brennecke <linus.brennecke@atos.net>
2020-12-02 07:51:03 +0100

    login initial

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,7 +18,38 @@ export class LoginComponent implements OnInit {
   passwort = "";
 
   submitLogin(): void {
-    // TODO
+    if (this.benutzerName == "") {
+      /* TODO
+      Implement some kind of error handling
+      */
+     console.log("kein name angegeben")
+     return;
+    }
+    else if (this.passwort == "") {
+      /* TODO
+      Implement some kind of error handling
+      */
+     console.log("passwort nicht angegeben")
+      return;
+    } else {
+      // TODO get url dynamically
+      fetch('http://localhost:3000/login', {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          stammName: this.benutzerName,
+          password: this.passwort
+        })
+      })
+      .then( (response) => {
+          // TODO
+          // route to homepage
+          console.log((response.body))
+      })
+    }
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -15,7 +15,49 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  name = "";
+  passwort = "";
+  passwort_confirm = "";
+  email = "";
+  adresse = "";
+  vorstand = "";
+
   registerClicked(): void{
-    this.myVar = true;
+    console.log("registrierungsanfrage")
+    if (this.name == "") {
+      /* TODO
+      Implement some kind of error handling
+      */
+     console.log("kein name angegeben")
+     return;
+    }
+    else if (this.passwort !== this.passwort_confirm) {
+      /* TODO
+      Implement some kind of error handling
+      */
+     console.log("passwort nicht gleich")
+      return;
+    } else {
+      // TODO get url dynamically
+      fetch('http://localhost:3000/register', {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.name,
+          passwort: this.passwort,
+          email: this.email,
+          ansprechpartner: this.vorstand
+        })
+      })
+      .then( (response) => {
+          // TODO
+          // route to homepage
+          console.log(response)
+      })
+    }
+
    }
 }
